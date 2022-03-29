2466e211fa10b8cb430d2b96ea4953300d7ebe87
QuangHieu1704 <52438446+QuangHieu1704@users.noreply.github.com>
2022-03-29 23:25:59 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+Hệ thống hỗ trợ nhập điểm thi tự động 
+
+- https://github.com/QuangHieu1704/He-thong-ho-tro-nhap-diem-thi-tu-dong
---
+++ main.py
@@ -0,0 +1,9 @@
+from recognition import load_model, doc_bang_diem, recognize_lopthi
+
+if __name__ == "__main__":
+    model = load_model()
+    recognize_lopthi(model)
+
+    
+
+
