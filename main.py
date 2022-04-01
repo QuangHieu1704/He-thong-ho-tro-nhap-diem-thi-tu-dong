675795d0e9ab2da1bbff231721da160f5e4f39e1
QuangHieu1704 <52438446+QuangHieu1704@users.noreply.github.com>
2022-04-01 15:55:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from recognition import load_model, doc_bang_diem, recognize_lopthi
+from recognition import load_model, doc_bang_diem, recognize_lopthi, recognize_folder
 
 if __name__ == "__main__":
     model = load_model()
     recognize_lopthi(model)
+    recognize_folder(model)
+    
 
     
 
