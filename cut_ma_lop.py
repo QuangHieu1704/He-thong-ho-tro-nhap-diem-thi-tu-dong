8d00003761e2010d21ea3e052582345ac52e9e96
QuangHieu1704 <52438446+QuangHieu1704@users.noreply.github.com>
2022-05-11 16:56:45 +0700

    Update cut_ma_lop.py

--- cut_ma_lop.py
+++ cut_ma_lop.py
@@ -4,6 +4,8 @@ import numpy as np
 import os
 import pytesseract
 
+pytesseract.pytesseract.tesseract_cmd = r"C:\\Program Files\\Tesseract-OCR"
+
 def find_ma_lop(target_img, ref_img):
     target_img = cv2.resize(target_img, (1653, 2337))
     width = target_img.shape[1]
@@ -14,19 +16,19 @@ def find_ma_lop(target_img, ref_img):
     matchLoc = maxLoc
     # cv2.rectangle(target_img, matchLoc, (matchLoc[0] + ref_img.shape[1], matchLoc[1] + ref_img.shape[0]), (0,0,0), 2, 8, 0 )
     # cv2.rectangle(result, matchLoc, (matchLoc[0] + ref_img.shape[1], matchLoc[1] + ref_img.shape[0]), (0,0,0), 2, 8, 0 )
-    # cv2.imshow("target ", target_img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    return result
+    img_malop = target_img[matchLoc[1] : matchLoc[1] + ref_img.shape[0], matchLoc[0] : matchLoc[0] + ref_img.shape[1]]
+
+    return img_malop
 
 if __name__ == "__main__":
-    ref_img = cv2.imread("He ho tro nhap diem tu dong\\Data\\test\\1-1.png", cv2.IMREAD_GRAYSCALE)
-    input_folder = "He ho tro nhap diem tu dong\\Data\\Bangdiem1"
-    output_folder = "He ho tro nhap diem tu dong\\Output\\Output cut ma lop"
+    ref_img = cv2.imread("Data\\test\\1-1.png", cv2.IMREAD_GRAYSCALE)
+    input_folder = "Data\\Bangdiem1"
+    output_folder = "Output\\Output cut ma lop"
     for filename in os.listdir(input_folder):
         print("Read ", os.path.join(input_folder, filename))
         target_img = cv2.imread(os.path.join(input_folder, filename), cv2.IMREAD_GRAYSCALE)
         img_malop = find_ma_lop(target_img, ref_img)
+
         text = pytesseract.image_to_string(img_malop)
         malop = re.search(r'\d\d\d\d\d\d', text)
         if malop:
