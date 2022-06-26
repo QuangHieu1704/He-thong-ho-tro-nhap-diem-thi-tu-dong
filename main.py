846ac1ca14bbfb4187a10fc9906333047fb4fa13
QuangHieu1704 <52438446+QuangHieu1704@users.noreply.github.com>
2022-06-26 17:15:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from recognition import load_model, doc_bang_diem, recognize_lopthi, recognize_folder
+from recognition import load_model, doc_bang_diem, recognize_file, recognize_folder
 
 if __name__ == "__main__":
     model = load_model()
-    recognize_lopthi(model)
-    recognize_folder(model)
-    
 
-    
+    file_path = "He ho tro nhap diem tu dong\\Data\\70001_0.png"
+    output_path = "He ho tro nhap diem tu dong\\Output"
+    recognize_file(model, file_path, output_path)
 
-
+    # input_path = "He ho tro nhap diem tu dong\\Data\\Bangdiem1"
+    # output_path = "He ho tro nhap diem tu dong\\Output"
+    # recognize_folder(model, input_path, output_path)
