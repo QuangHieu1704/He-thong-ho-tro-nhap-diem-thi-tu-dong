b8175eafcb8c57165bb32239afa3dbb09ed8d416
QuangHieu1704 <52438446+QuangHieu1704@users.noreply.github.com>
2022-06-26 17:14:50 +0700

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -14,15 +14,15 @@ def average(list):
     return round(sum(list) / len(list))
 
 
-def clustering_coordinates(listCoor, label, height, width):
+def clustering_coordinates(list_coor, label, height, width):
     if label == 0:
         max_distance = round(width * 0.03)  # vertical line
     if label == 1:
         max_distance = round(height * 0.008) # horizontal line
     cluster = []
-    curr_point = listCoor[0]
+    curr_point = list_coor[0]
     curr_cluster = [curr_point]
-    for x in listCoor[1:]:
+    for x in list_coor[1:]:
         if x <= curr_point + max_distance:
             curr_cluster.append(x)
         else:
@@ -59,7 +59,6 @@ def correct_skew(image, delta=1, limit=5):
 
 
 def img_to_binary(img):
-    # blur = cv2.GaussianBlur(img, (3,3), 0)   # Lọc Gauss loại bỏ nhiễu
     thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]  # Phân ngưỡng về ảnh đen trắng
     return thresh
 
@@ -69,19 +68,19 @@ def get_lines(img):
     img = img_to_binary(img)
     horiznotal_coor = []  # Các tọa độ ngang  y
     vertical_coor = []  # Các tọa độ thẳng  x
-    SE_HorizontalSize = round(width * 0.04)
-    SE_VerticalSize = round(height * 0.04)
+    se_horizontal_size = round(width * 0.04)
+    se_vertical_size = round(height * 0.04)
     # Detect horizontal lines
-    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (SE_HorizontalSize, 1))
-    horizontal_mask = cv2.morphologyEx(img, cv2.MORPH_HITMISS, horizontal_kernel, iterations=1)
-    lines = cv2.HoughLinesP(horizontal_mask, 1, np.pi / 180, 0, minLineLength=SE_HorizontalSize, maxLineGap=1)
+    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (se_horizontal_size, 1))
+    horizontal_mask = cv2.morphologyEx(img, cv2.MORPH_HITMISS, horizontal_kernel, iterations = 1)
+    lines = cv2.HoughLinesP(horizontal_mask, 1, np.pi / 180, 0, minLineLength = se_horizontal_size, maxLineGap=1)
     for line in lines:
         x1, y1, x2, y2 = line[0]
         horiznotal_coor.append(y1)
         # Detect vertical lines
-    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, SE_VerticalSize))
+    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, se_vertical_size))
     vertical_mask = cv2.morphologyEx(img, cv2.MORPH_HITMISS, vertical_kernel, iterations=1)
-    lines = cv2.HoughLinesP(vertical_mask, 1, np.pi / 180, 0, minLineLength=1, maxLineGap=SE_VerticalSize)
+    lines = cv2.HoughLinesP(vertical_mask, 1, np.pi / 180, 0, minLineLength = 1, maxLineGap=se_vertical_size)
     for line in lines:
         x1, y1, x2, y2 = line[0]
         vertical_coor.append(x1)
@@ -96,4 +95,14 @@ def get_lines(img):
         new_horizontal_coor = np.delete(new_horizontal_coor, 0)
 
     return new_horizontal_coor, new_vertical_coor
+
+
+def image_matching(target_img, ref_img):
+    result = cv2.matchTemplate(target_img, ref_img, cv2.TM_CCOEFF_NORMED)
+    cv2.normalize(result, result, 0, 1, cv2.NORM_MINMAX, -1)
+    _, _, _, max_loc = cv2.minMaxLoc(result, None)
+    match_loc = max_loc
+    img_malop = target_img[match_loc[1] : match_loc[1] + ref_img.shape[0], match_loc[0] : match_loc[0] + ref_img.shape[1]]
+
+    return img_malop
 
